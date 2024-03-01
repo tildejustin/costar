743867d025e8d454d28af73dbf02e011f702298f
tildejustin <spacepepper3.14@gmail.com>
2024-02-29 21:11:40 -0500

    functionality

---
+++ src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
@@ -0,0 +1,15 @@
+package dev.tildejustin.costar.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+
+@Mixin(World.class)
+public abstract class WorldMixin {
+    @Redirect(method = "tickEntities", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
+    private String remapBlockEntityClassName(Class<BlockEntity> instance) {
+        return FabricLoader.getInstance().getMappingResolver().unmapClassName("official", instance.getCanonicalName());
+    }
+}
