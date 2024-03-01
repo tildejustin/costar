dcdc75e7507f982a4fea9627eadca4400fec8414
tildejustin <spacepepper3.14@gmail.com>
2024-02-29 21:16:21 -0500

    port to v2 intermediaries

--- src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
+++ src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
-    @Redirect(method = "tickEntities", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
+    @Redirect(method = "method_8429", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
     private String remapBlockEntityClassName(Class<BlockEntity> instance) {
         return FabricLoader.getInstance().getMappingResolver().unmapClassName("official", instance.getCanonicalName());
     }
