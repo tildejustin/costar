fe8efbec0dc1736872cd5fd36440221e21a781cf
tildejustin <spacepepper3.14@gmail.com>
2024-03-01 08:41:09 -0500

    be more faithful to the result of Class#getSimpleName

--- src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
+++ src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
@@ -1,15 +1,19 @@
 package dev.tildejustin.costar.mixin;
 
-import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
+    @Unique
+    private static final MappingResolver mappingResolver = FabricLoader.getInstance().getMappingResolver();
+
     @Redirect(method = "method_8429", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
     private String remapBlockEntityClassName(Class<BlockEntity> instance) {
-        return FabricLoader.getInstance().getMappingResolver().unmapClassName("official", instance.getCanonicalName());
+        String className = mappingResolver.unmapClassName("official", instance.getName());
+        return className.substring(className.lastIndexOf(".") + 1);
     }
 }
