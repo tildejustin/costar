d84e8a7f11df85fdd1bf0ea504d8c8d1a134fec8
tildejustin <spacepepper3.14@gmail.com>
2024-03-02 19:17:59 -0500

    make it really faithful to Class#getSimpleName

--- src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
+++ src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
@@ -13,7 +13,27 @@ public abstract class WorldMixin {
 
     @Redirect(method = "tickEntities", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
     private String remapBlockEntityClassName(Class<BlockEntity> instance) {
-        String className = mappingResolver.unmapClassName("official", instance.getName());
-        return className.substring(className.lastIndexOf(".") + 1);
+        // big thanks to https://github.com/openjdk/jdk8/blob/6a383433a9f4661a96a90b2a4c7b5b9a85720031/jdk/src/share/classes/java/lang/Class.java#L1292-L1323
+        String className = instance.getName();
+        String remappedClassName = mappingResolver.unmapClassName("official", className);
+        Class<?> enclosingClass = instance.getEnclosingClass();
+        // top level class case
+        if (enclosingClass == null) {
+            return remappedClassName.substring(remappedClassName.lastIndexOf(".") + 1);
+        }
+        String enclosingClassName = enclosingClass.getName();
+        String remappedEnclosingClassName = mappingResolver.unmapClassName("official", enclosingClassName);
+        String remappedSimpleBinaryName = remappedClassName.substring(remappedEnclosingClassName.length());
+        int length = remappedSimpleBinaryName.length();
+        int index = 1;
+        while (index < length && isAsciiDigit(remappedSimpleBinaryName.charAt(index))) {
+            ++index;
+        }
+        return remappedSimpleBinaryName.substring(index);
+    }
+
+    @Unique
+    private static boolean isAsciiDigit(char c) {
+        return '0' <= c && c <= '9';
     }
 }
