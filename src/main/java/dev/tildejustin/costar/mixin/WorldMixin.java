4b9684bc34429e7c7083b86e8e08fb35042c7176
tildejustin <spacepepper3.14@gmail.com>
2024-05-10 17:28:42 -0400

    rewrite for complete parity

--- src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
+++ src/main/java/dev/tildejustin/costar/mixin/WorldMixin.java
@@ -13,25 +13,81 @@ public abstract class WorldMixin {
 
     @Redirect(method = "method_8429", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
     private String remapBlockEntityClassName(Class<BlockEntity> instance) {
-        // big thanks to https://github.com/openjdk/jdk8/blob/6a383433a9f4661a96a90b2a4c7b5b9a85720031/jdk/src/share/classes/java/lang/Class.java#L1292-L1323
-        String className = instance.getName();
-        String remappedClassName = mappingResolver.unmapClassName("official", className);
-        Class<?> enclosingClass = instance.getEnclosingClass();
-        // top level class case
-        if (enclosingClass == null) {
-            return remappedClassName.substring(remappedClassName.lastIndexOf(".") + 1);
-        }
-        String enclosingClassName = enclosingClass.getName();
-        String remappedEnclosingClassName = mappingResolver.unmapClassName("official", enclosingClassName);
-        String remappedSimpleBinaryName = remappedClassName.substring(remappedEnclosingClassName.length());
-        int length = remappedSimpleBinaryName.length();
-        int index = 1;
-        while (index < length && isAsciiDigit(remappedSimpleBinaryName.charAt(index))) {
-            ++index;
-        }
-        return remappedSimpleBinaryName.substring(index);
+        return getRemappedSimpleName(instance);
     }
 
+    // the following is edited from https://github.com/openjdk/jdk8/blob/6a383433a9f4661a96a90b2a4c7b5b9a85720031/jdk/src/share/classes/java/lang/Class.java#L1292-L1323
+
+    /**
+     * Returns the simple name of the underlying class as given in the
+     * source code. Returns an empty string if the underlying class is
+     * anonymous.
+     *
+     * <p>The simple name of an array is the simple name of the
+     * component type with "[]" appended.  In particular the simple
+     * name of an array whose component type is anonymous is "[]".
+     *
+     * @return the simple name of the underlying class
+     * @since 1.5
+     */
+    @Unique
+    private String getRemappedSimpleName(Class<?> instance) {
+        if (instance.isArray())
+            return getRemappedSimpleName(instance.getComponentType())+"[]";
+
+        String simpleName = getRemappedSimpleBinaryName(instance);
+        if (simpleName == null) { // top level class
+            simpleName = mappingResolver.unmapClassName("official", instance.getName());
+            return simpleName.substring(simpleName.lastIndexOf(".")+1); // strip the package name
+        }
+        // According to JLS3 "Binary Compatibility" (13.1) the binary
+        // name of non-package classes (not top level) is the binary
+        // name of the immediately enclosing class followed by a '$' followed by:
+        // (for nested and inner classes): the simple name.
+        // (for local classes): 1 or more digits followed by the simple name.
+        // (for anonymous classes): 1 or more digits.
+
+        // Since getSimpleBinaryName() will strip the binary name of
+        // the immediatly enclosing class, we are now looking at a
+        // string that matches the regular expression "\$[0-9]*"
+        // followed by a simple name (considering the simple of an
+        // anonymous class to be the empty string).
+
+        // Remove leading "\$[0-9]*" from the name
+        int length = simpleName.length();
+        if (length < 1 || simpleName.charAt(0) != '$')
+            throw new InternalError("Malformed class name");
+        int index = 1;
+        while (index < length && isAsciiDigit(simpleName.charAt(index)))
+            index++;
+        // Eventually, this is the empty string iff this is an anonymous class
+        return simpleName.substring(index);
+    }
+
+    /**
+     * Returns the "simple binary name" of the underlying class, i.e.,
+     * the binary name without the leading enclosing class name.
+     * Returns {@code null} if the underlying class is a top level
+     * class.
+     */
+    @Unique
+    private String getRemappedSimpleBinaryName(Class<?> instance) {
+        Class<?> enclosingClass = instance.getEnclosingClass();
+        if (enclosingClass == null) // top level class
+            return null;
+        // Otherwise, strip the enclosing class' name
+        try {
+            // return getName().substring(enclosingClass.getName().length());
+            return mappingResolver.unmapClassName("official", instance.getName()).substring(mappingResolver.unmapClassName("official", enclosingClass.getName()).length());
+        } catch (IndexOutOfBoundsException ex) {
+            throw new InternalError("Malformed class name", ex);
+        }
+    }
+
+    /**
+     * Character.isDigit answers {@code true} to some non-ascii
+     * digits.  This one does not.
+     */
     @Unique
     private static boolean isAsciiDigit(char c) {
         return '0' <= c && c <= '9';
